14af7cb66ca64619c49cd78ecdb416b66522cac4
adm <kaio.candf@gmail.com>
2026-06-30 22:41:49 -0300

    estudando logica com exercicios

--- exercicios/exercicios55.php
+++ exercicios/exercicios55.php
@@ -1,6 +1,5 @@
 <?php
 
-<<<<<<< HEAD
 class ProteinTranslation
 {
     public function getProteins(string $strand): array
@@ -70,53 +69,5 @@ class ProteinTranslation
         }
 
         return $resultFinal;
-=======
-class House
-{
-    private $frases = [
-    "the house that Jack built.",
-    "the malt\nthat lay in",
-    "the rat\nthat ate",
-    "the cat\nthat killed",
-    "the dog\nthat worried",
-    "the cow with the crumpled horn\nthat tossed",
-    "the maiden all forlorn\nthat milked",
-    "the man all tattered and torn\nthat kissed",
-    "the priest all shaven and shorn\nthat married",
-    "the rooster that crowed in the morn\nthat woke",
-    "the farmer sowing his corn\nthat kept",
-    "the horse and the hound and the horn\nthat belonged to",
-    ];
-
-    public function verse(int $verseNumber): array
-    {
-        $fraseNova = array_slice($this->frases, 0, $verseNumber);
-
-        $fraseNova = array_reverse($fraseNova);
-
-        $texto = "This is " . implode(" ", $fraseNova);
-
-        $texto = explode("\n", $texto);
-
-        return $texto;
-
-    }
-
-    public function verses(int $start, int $end): array
-    {
-        $fraseNova = [];
-
-        for($i = $start; $i <= $end; $i++){
-            foreach($this->verse($i) as $linha){
-                $fraseNova[] = $linha;
-            }
-
-            if($i<$end){
-                $fraseNova[] = '';
-            }
-        }
-        
-        return $fraseNova;
->>>>>>> fc6cda63c4850517398b46b83f861d607c1001e6
     }
 }
